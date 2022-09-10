531b47fbcd04b2e393b83015b1aaca8a089ef188
Godnon Dsilva <godnondsilva@gmail.com>
2022-09-10 11:14:18 +0530

    Fix index page and header responsiveness

--- js/script.js
+++ js/script.js
@@ -11,17 +11,6 @@ menuBtn.addEventListener("click", () => {
 let navBar = document.querySelector("header");
 let scrollPos = 0;
 
-window.addEventListener("scroll", () => {
-  let scrollPosNow = window.pageYOffset || document.pageYOffset;
-
-  if (scrollPosNow > scrollPos) {
-    navBar.style.transform = "translateY(-100%)";
-  } else {
-    navBar.style.transform = "translateY(0)";
-  }
-
-  scrollPos = scrollPosNow;
-});
 
 // Add Your Scripts Here:
 // <Topic/Explain> ******************************
